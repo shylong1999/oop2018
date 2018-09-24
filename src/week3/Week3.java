f3cccd338172120014fba819140aecca15b24154
Long <unglong5@gmail.com>
2018-09-24 22:47:15 +0700

    update

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -3,13 +3,15 @@ package week3;
 public class Week3 {
 
     public static int max(int m, int n) {
-        // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        return Math.max(m,n);
     }
 
     public static int minOfArray(int[] array) {
-        // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int min = 99999;
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i] < min) min = array[i];
+        }
+        return min;
     }
 
     /**
@@ -19,7 +21,22 @@ public class Week3 {
      * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
      */
     public static String calculateBMI(double weight, double height) {
-        // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        double BMI;
+        BMI = weight / (height*height);
+        if (BMI < 18.5) return "Thiếu cân";
+        else if (BMI >= 18.5 && BMI <= 22.99) return "Bình thường";
+        else if (BMI >= 23 && BMI <= 24.99) return "Thừa cân";
+        return "Béo phì";
     }
+
+    public static void main(String[] args) {
+        System.out.println(max(5,10));
+
+        int a[] = {1, 2, 3, 4, 99 , -1 , -6, -10};
+        System.out.println(minOfArray(a));
+
+        System.out.println(calculateBMI(67,1.70));
+    }
+
+
 }
