c6224ef3c808787b9f8c511400af5fcad2a6a69c
Long <unglong5@gmail.com>
2018-09-26 16:58:36 +0700

    update

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,6 +1,6 @@
 package week2.task2;
 
-    public class Fraction {
+public class Fraction {
 
         private int numerator,denominator;
 
@@ -50,59 +50,65 @@ package week2.task2;
         }
 
 
-    public void Reduce(){
-        int UCLN = gcd(numerator,denominator);
-        this.numerator = this.numerator/UCLN;
-        this.denominator = this.denominator/UCLN;
-    }
+        public void Reduce(){
+            int UCLN = gcd(numerator,denominator);
+            this.numerator = this.numerator/UCLN;
+            this.denominator = this.denominator/UCLN;
+        }
 
-    public boolean equals(Fraction obj){
-        int tu =  this.numerator*obj.denominator - this.denominator*obj.numerator;
-        int mau =  this.denominator*obj.denominator;
+        public boolean equals(Object obj){
+            if (obj == null) return false;
 
-        if (tu==0) System.out.println("Hai phan so bang nhau");
+            if (!(obj instanceof Fraction)) return false; //kiểm tra Object có phải kiểu Fraction hay không
+            Fraction fraction = (Fraction) obj; //ép kiểu Object
 
-        if (tu * mau >=0) return true;
-        return false;
-    }
-    public static void main(String[] args) {
-        Fraction ps = new Fraction(3,5);
-        Fraction ps1 = new Fraction(10,15);
-        Fraction ps2 = new Fraction(9,15);
-        ps.PrintFraction();
+            int tu =  this.numerator*fraction.denominator - this.denominator*fraction.numerator;
+            int mau =  this.denominator*fraction.denominator;
 
-        System.out.println("Rút gọn phân số 10/15");
-        ps1.Reduce();
-        ps1.PrintFraction();
+            if (tu==0) System.out.println("Hai phan so bang nhau");
 
-        System.out.println("Phép cộng của 3/5 với 10/15");
-        Fraction cong = ps.add(ps1);
-        cong.Reduce();
-        cong.PrintFraction();
+            if (tu * mau >=0) return true;
+            return false;
+        }
+        public static void main(String[] args) {
 
-        System.out.println("Phép trừ của 3/5 với 10/15");
-        Fraction tru = ps.subtract(ps1);
-        tru.Reduce();
-        tru.PrintFraction();
+            Fraction ps = new Fraction(3,5);
+            Fraction ps1 = new Fraction(10,15);
+            Fraction ps2 = new Fraction(9,15);
+            ps.PrintFraction();
 
-        System.out.println("Phép nhan của 3/5 với 10/15");
-        Fraction nhan = ps.multiply(ps1);
-        nhan.Reduce();
-        nhan.PrintFraction();
+            System.out.println("Rút gọn phân số 10/15");
+            ps1.Reduce();
+            ps1.PrintFraction();
 
-        System.out.println("Phép chia của 3/5 với 10/15");
-        Fraction chia = ps.divide(ps1);
-        chia.Reduce();
-        chia.PrintFraction();
+            System.out.println("Phép cộng của 3/5 với 10/15");
+            Fraction cong = ps.add(ps1);
+            cong.Reduce();
+            cong.PrintFraction();
 
-        System.out.println("So sánh 2 phân số 3/5 và 10/15");
-        if (ps.equals(ps1)) System.out.println("True");
-        else System.out.println("False");
+            System.out.println("Phép trừ của 3/5 với 10/15");
+            Fraction tru = ps.subtract(ps1);
+            tru.Reduce();
+            tru.PrintFraction();
 
-        System.out.println("So sánh 2 phân số 3/5 và 9/15");
-        if (!ps.equals(ps2)) System.out.println("True");
-        else System.out.println("True");
+            System.out.println("Phép nhan của 3/5 với 10/15");
+            Fraction nhan = ps.multiply(ps1);
+            nhan.Reduce();
+            nhan.PrintFraction();
 
-    }
+            System.out.println("Phép chia của 3/5 với 10/15");
+            Fraction chia = ps.divide(ps1);
+            chia.Reduce();
+            chia.PrintFraction();
+
+            System.out.println("So sánh 2 phân số 3/5 và 10/15");
+            if (ps.equals(ps1)) System.out.println("True");
+            else System.out.println("False");
+
+            System.out.println("So sánh 2 phân số 3/5 và 9/15");
+            if (ps.equals(ps2)) System.out.println("True");
+            else System.out.println("False");
+
+        }
 
 }
